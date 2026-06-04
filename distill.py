b6eb2e9c563b1ac30003ee0d64d24bd84d92f7bb
ninfueng <ninnart.fuengfusin@yahoo.com>
2026-06-04 19:24:21 +0900

    feat(distill): support weighted losses

--- distill.py
+++ distill.py
@@ -1,14 +1,15 @@
 """Try to train FitNet."""
 
-from datetime import datetime
 import argparse
+import logging
 import os
 import time
-import logging
+from datetime import datetime
 
 import torch
-import torchvision.transforms as T
 import torch.backends.cudnn as cudnn
+import torch.nn.functional as F
+import torchvision.transforms as T
 from timm.utils.metrics import AverageMeter, accuracy
 from torch import Tensor, nn
 from torch.optim import SGD
@@ -30,6 +31,9 @@ def set_forward_manager(
 
     forward_manager = ForwardHookManager(device)
     for teacher_layer in teacher_student_layer_map.keys():
+        if teacher_layer == 'base':
+            continue
+
         student_layer, _, _, _ = teacher_student_layer_map[teacher_layer]
         forward_manager.add_hook(
             teacher_model, teacher_layer, requires_input=False, requires_output=True
@@ -44,20 +48,22 @@ def get_distill_losses(
     forward_manager: ForwardHookManager,
     teacher_student_layer_map: dict[str, list[str | nn.Module]],
 ) -> list[Tensor]:
+    io_dict = forward_manager.pop_io_dict()
 
     distill_losses = []
     for teacher_layer in teacher_student_layer_map.keys():
-        student_layer, feature_extractor, loss, lambda_fn = teacher_student_layer_map[
+        if teacher_layer == 'base':
+            continue
+
+        student_layer, feature_extractor, loss, weight = teacher_student_layer_map[
             teacher_layer
         ]
 
-        io_dict = forward_manager.pop_io_dict()
         teacher_out = io_dict[teacher_layer]['output']
         student_out = io_dict[student_layer]['output']
 
         student_out = feature_extractor(student_out)
-        distill_loss = loss(student_out, teacher_out)
-        distill_loss = lambda_fn(distill_loss)
+        distill_loss = weight * loss(student_out, teacher_out)
         distill_losses.append(distill_loss)
     return distill_losses
 
@@ -68,6 +74,9 @@ def parse_auxiliary_module(
 
     auxiliary_map = {}
     for teacher_layer in teacher_student_layer_map.keys():
+        if teacher_layer == 'base':
+            continue
+
         student_layer, feature_extractor, _, _ = teacher_student_layer_map[
             teacher_layer
         ]
@@ -79,8 +88,8 @@ def parse_auxiliary_module(
 
 
 def train_distill(
-    student_model: nn.Module,
     teacher_model: nn.Module,
+    student_model: nn.Module,
     loader: DataLoader,
     criterion: nn.modules.loss._Loss,
     optimizer: torch.optim.Optimizer,
@@ -89,11 +98,13 @@ def train_distill(
     teacher_student_layer_map: dict,
     device,
 ) -> None:
-    student_model.train()
     teacher_model.eval()
+    student_model.train()
 
-    student_top1, student_losses = AverageMeter(), AverageMeter()
     teacher_top1, teacher_losses = AverageMeter(), AverageMeter()
+    student_top1, student_losses = AverageMeter(), AverageMeter()
+
+    cross_entropy_weight = teacher_student_layer_map['base'][-1]
 
     start = time.perf_counter()
 
@@ -107,9 +118,14 @@ def train_distill(
             teacher_loss = criterion(teacher_output, target)
 
         student_loss = criterion(student_output, target)
+        student_loss *= cross_entropy_weight
+
         distill_losses = get_distill_losses(forward_manager, teacher_student_layer_map)
+        # NOTE: distill weights included in get_distill_losses().
+        all_distill_loss = 0.0
         for distill_loss in distill_losses:
-            student_loss += distill_loss
+            all_distill_loss += distill_loss
+        student_loss += all_distill_loss
 
         optimizer.zero_grad()
         student_loss.backward()
@@ -118,8 +134,8 @@ def train_distill(
         with torch.no_grad():
             size = input.size(0)
 
-            student_prec1 = accuracy(student_output.data, target)[0]
             teacher_prec1 = accuracy(teacher_output.data, target)[0]
+            student_prec1 = accuracy(student_output.data, target)[0]
 
             student_losses.update(student_loss.item(), size)
             student_top1.update(student_prec1.item(), size)
@@ -133,14 +149,36 @@ def train_distill(
     )
 
 
+def logit_distillation(
+    teacher_logits: Tensor,
+    student_logits: Tensor,
+    T: float,
+) -> Tensor:
+    soft_teacher_logits = F.softmax(teacher_logits / T, dim=-1)
+    soft_student_logits = F.log_softmax(student_logits / T, dim=-1)
+
+    # NOTE: cross entropy between teacher to teacher - student
+    distill_loss = (
+        (T**2.0)
+        * soft_teacher_logits
+        * (soft_teacher_logits.log() - soft_student_logits)
+    )
+    distill_loss = distill_loss.mean()
+    return distill_loss
+
+
+class LogitDistillationLoss(nn.Module):
+    def __init__(self, T: float) -> None:
+        super().__init__()
+        self.T = T
+
+    def forward(self, teacher_logits: Tensor, student_logits: Tensor) -> Tensor:
+        distill_loss = logit_distillation(teacher_logits, student_logits, self.T)
+        return distill_loss
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--student-arch', type=str, default='resnet20')
-    parser.add_argument('--teacher-arch', type=str, default='resnet110')
-
-    parser.add_argument('--student-dir', type=str, default='resnet20.pt')
-    parser.add_argument('--teacher-dir', type=str, default='resnet110.pt')
-
     parser.add_argument('--workers', type=int, default=8)
     parser.add_argument('--epochs', type=int, default=200)
     parser.add_argument('--batch-size', type=int, default=128)
@@ -149,15 +187,36 @@ if __name__ == '__main__':
     parser.add_argument('--weight-decay', type=float, default=1e-4)
     parser.add_argument('--exp-dir', type=str, default='exps')
     parser.add_argument('--dataset-dir', type=str, default='./data')
+
+    parser.add_argument('--student-arch', type=str, default='resnet20')
+    parser.add_argument('--teacher-arch', type=str, default='resnet110')
+    parser.add_argument('--student-dir', type=str, default='')
+    parser.add_argument('--teacher-dir', type=str, default='resnet110.pt')
+    parser.add_argument('--T', type=float, default=2.0)
     args = parser.parse_args()
 
-    # NOTE: teacher_layer: [student_layer, feature_extractor, loss, lambda]
+    # NOTE: teacher_layer: [student_layer, feature_extractor, loss]
     TEACHER_STUDENT_LAYER_MAP = {
+        # cross entropy weights
+        'base': [
+            None,
+            None,
+            None,
+            3 / 4,
+        ],
+        # feature-based
         'layer3.17': [
             'layer3.0',
             nn.Identity(),
             nn.MSELoss(reduction='mean'),
-            lambda x: x * 1e-3,
+            1 / 8,
+        ],
+        # logit-based
+        'linear': [
+            'linear',
+            nn.Identity(),
+            LogitDistillationLoss(T=args.T),
+            1 / 4,
         ],
     }
     auxiliary_map = parse_auxiliary_module(TEACHER_STUDENT_LAYER_MAP)
@@ -178,8 +237,14 @@ if __name__ == '__main__':
         ],
     )
 
-    student_dir = os.path.expanduser(args.student_dir)
     teacher_dir = os.path.expanduser(args.teacher_dir)
+    student_dir = os.path.expanduser(args.student_dir)
+
+    teacher_state_dict = None
+    if os.path.isfile(teacher_dir):
+        teacher_state_dict = torch.load(teacher_dir, weights_only=False)['state_dict']
+    else:
+        raise NotImplementedError('')
 
     student_state_dict = None
     if os.path.isfile(student_dir):
@@ -187,27 +252,21 @@ if __name__ == '__main__':
     else:
         logging.warning(f'student_dir is not found. Your: {student_dir}')
 
-    teacher_state_dict = None
-    if os.path.isfile(teacher_dir):
-        teacher_state_dict = torch.load(teacher_dir, weights_only=False)['state_dict']
-    else:
-        raise NotImplementedError()
-
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    student_model = resnet.__dict__[args.student_arch]()
     teacher_model = resnet.__dict__[args.teacher_arch]()
-
-    if student_state_dict is not None:
-        student_model.load_state_dict(student_state_dict)
+    student_model = resnet.__dict__[args.student_arch]()
 
     if teacher_state_dict is not None:
         teacher_model.load_state_dict(teacher_state_dict)
 
+    if student_state_dict is not None:
+        student_model.load_state_dict(student_state_dict)
+
     for name, module in auxiliary_map.items():
         student_model.add_module(name, module)
 
-    student_model = student_model.to(device)
     teacher_model = teacher_model.to(device)
+    student_model = student_model.to(device)
 
     train_transforms = T.Compose(
         [
@@ -279,8 +338,8 @@ if __name__ == '__main__':
     best_student_prec1 = best_student_epoch = 0
     for epoch in range(1, args.epochs + 1):
         train_distill(
-            student_model,
             teacher_model,
+            student_model,
             train_loader,
             criterion,
             optimizer,
